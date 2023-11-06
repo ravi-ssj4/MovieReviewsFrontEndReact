aaa4511e995be3d8ce433e7af60cb5227623435d
Ravi Ranjan <raviranjan.ssj4@gmail.com>
2023-11-06 22:00:47 +0530

    Designed the Hero component, added header component and Trailer component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import api from './api/axiosConfig';
 import { useEffect, useState } from 'react';
 import Layout from './components/Layout';
 import { Route, Routes } from 'react-router-dom';
-
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 function App() {
   const [movies, setMovies] = useState();
 
@@ -13,6 +15,9 @@ function App() {
       const response = await api.get('/api/v1/movies');
       // Todo: check for status code for response and appropriately handle
       console.log(response.data);
+      if (response.status != 200){
+        console.log("error");
+      }
       // we are updating the state of the movies variable 
       // -> our component will re-render hence!
       setMovies(response.data);
@@ -29,9 +34,11 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path='/' element={Layout}>
-
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />}></Route>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}></Route>
         </Route>
       </Routes>
     </div>
--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://9501-2401-4900-1cbc-6e87-c5d2-cc86-9d85-865f.ngrok-free.app',
+    baseURL: 'https://eed9-2401-4900-1cbd-7a33-386d-46c8-4971-c7ac.ngrok-free.app',
     headers: {'ngrok-skip-browser-warning': 'true'} // to avoid CORS - the server is hosted via ngrok(a different domain)
 });
